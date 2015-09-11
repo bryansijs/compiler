50165712993e167646cd91420bdd1d005f52a7da
bryansijs <bryansijs@gmail.com>
2015-09-11 09:40:17 +0200

    hello world

---
+++ src/compiler/Main.java
@@ -0,0 +1,45 @@
+package compiler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+/*
+ VARIABELE
+lade x = “hoi”
+
+CONDITIE
+
+als (x == “hoi”){
+	
+}
+alsanders{
+
+}
+anders{
+
+}
+
+LOOP
+zolang(x = true){
+
+}
+
+voor{
+}
+
+OPERATORS
++-/: DEFAULT
+
+COMMENT
+gelul
+	dit is commentaar
+eindgelul
+
+lees: dit is een enkele regel.
+*/
